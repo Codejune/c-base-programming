9590cd1f54a91ac7bdd2b5d4c65ef53b6893ae85
Codejune <Codejune@MacBook-Pro.local>
2016-07-11 10:59:10 +0900

    Add Chapter 8

---
+++ Chapter8/8-2.c
@@ -0,0 +1,19 @@
+/*
+    다음 프로그램을 실행한 후 결과가 어떻게 나오는가? 결과가 제대로 나오지 않는다면 왜 그런지 이유를 쓰고 수정하라.
+
+    #include <stdio.h>
+    void add(int, int, int);
+    int main(void){
+        int x, y, result;
+        printf("두 수를 입력하시오 : ");
+        scanf("%d %d", &x, &y);
+        add(x, y, result);
+        printf("%d + %d = %d입니다.\n", x, y, result);
+        return 0;
+    }
+    
+    void add(int a, int b, int c);
+        c = a + b;
+    }
+*/
+ 
